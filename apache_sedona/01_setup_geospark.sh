9da043b45fb72f4a8680522c9dd983dd81f7485a
Hugh Saalmans <hugh.saalmans@iag.com.au>
2020-09-27 20:05:50 +1000

    Initial version

---
+++ apache_sedona/01_setup_geospark.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/env bash
+
+SECONDS=0*
+
+echo "-------------------------------------------------------------------------"
+echo " Start time : $(date)"
+
+# --------------------------------------------------------------------------------------------------------------------
+# Script installs Apache Sedona (formerly Geospark) locally on a Mac with Spark in standalone mode
+# --------------------------------------------------------------------------------------------------------------------
+#
+# Author: Hugh Saalmans, IAG Strategy & Innovation
+# Date: 2020-09-25
+#
+# WARNINGS:
+#   - Removes existing Spark install in $HOME/spark-$SPARK_VERSION-bin-hadoop2.7 folder
+#   - Removes existing 'geospark_env' Conda environment
+#
+# PRE_REQUISITES:
+#   1. Run script off the VPN/corporate network to avoid proxy server issues
+#   2. Java SE SDK is installed and linked to /Library/Java/Home
+#        - Install Java 8 using Homebrew: brew cask install java8; OR
+#        - Install Java 11: brew cask install java11
+#   2. Miniconda installed in default directory ($HOME/opt/miniconda3)
+#        - Get the installer here: https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.pkg
+#
+# ISSUES:
+#   1. Conda environment variables aren't accessible in IntelliJ/Pycharm due to a missing feature
+#        - Geospark python scripts will fail in IntelliJ/Pycharm as Spark env vars aren't set (e.g. $SPARK_HOME)
+#
+# --------------------------------------------------------------------------------------------------------------------
+#
+# SETUP:
+#   - edit these if you know what versions go with what version!?
+#       - for the current version of Geospark (1.3.1) and Conda: only Python 3.7 and Spark 2.4.6 are a valid combination
+#
+PYTHON_VERSION="3.7"
+SPARK_VERSION="2.4.6"
+#
+# --------------------------------------------------------------------------------------------------------------------
+
+# get directory this script is running from
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+SPARK_HOME="${HOME}/spark-${SPARK_VERSION}-bin-hadoop2.7"
+
+# WARNING - remove existing spark install
+rm -r ${SPARK_HOME}
+
+echo "-------------------------------------------------------------------------"
+echo "Downloading and Installing Apache Spark"
+echo "-------------------------------------------------------------------------"
+
+cd ${HOME}
+
+# download and untar Spark files
+wget https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoop2.7.tgz
+tar -xzf spark-${SPARK_VERSION}-bin-hadoop2.7.tgz
+rm spark-${SPARK_VERSION}-bin-hadoop2.7.tgz
+
+# add Postgres JDBC driver to Spark (optional - included for running xx_prep_abs_boundaries.py)
+cd ${SPARK_HOME}/jars
+wget https://jdbc.postgresql.org/download/postgresql-42.2.16.jar
+
+echo "-------------------------------------------------------------------------"
+echo "Creating new Conda Environment 'geospark_env'"
+echo "-------------------------------------------------------------------------"
+
+# stop the Conda environment currently running
+conda deactivate
+
+# update Conda platform
+echo "y" | conda update conda
+
+# WARNING - remove existing environment
+conda env remove --name geospark_env
+
+# Create Conda environment
+echo "y" | conda create -n geospark_env python=${PYTHON_VERSION}
+
+# activate and setup env
+conda activate geospark_env
+conda config --env --add channels conda-forge
+conda config --env --set channel_priority strict
+
+# add environment variables
+conda env config vars set JAVA_HOME="/Library/Java/Home"
+conda env config vars set SPARK_HOME="${SPARK_HOME}"
+conda env config vars set SPARK_LOCAL_IP="127.0.0.1"
+conda env config vars set PYSPARK_PYTHON="${HOME}/opt/miniconda3/envs/geospark_env/bin/python"
+conda env config vars set PYSPARK_DRIVER_PYTHON="${HOME}/opt/miniconda3/envs/geospark_env/bin/python"
+conda env config vars set PYLIB="${SPARK_HOME}/python/lib"
+
+# reactivate for env vars to take effect
+conda activate geospark_env
+
+# install packages for geospark
+echo "y" | conda install -c conda-forge pyspark=${SPARK_VERSION} jupyter
+pip install geospark
+
+echo "-------------------------------------------------------------------------"
+echo "Verify Geospark version"
+echo "-------------------------------------------------------------------------"
+
+# confirm version of Geospark installed
+conda list geospark
+
+echo "-------------------------------------------------------------------------"
+echo "Run test Geospark script to prove everything is working"
+echo "-------------------------------------------------------------------------"
+
+python ${SCRIPT_DIR}/02_run_spatial_query.py
+
+echo "----------------------------------------------------------------------------------------------------------------"
+
+duration=$SECONDS
+
+echo " End time : $(date)"
+echo " it took $((duration / 60)) mins"
+echo "----------------------------------------------------------------------------------------------------------------"
---
+++ apache_sedona/xx_prep_abs_boundaries.py
@@ -0,0 +1,159 @@
+
+# script to load boundary & point data from Postgres and export it to parquet format for later use in a Spark script
+
+import logging
+import os
+import sys
+
+from datetime import datetime
+from multiprocessing import cpu_count
+from pyspark.sql import SparkSession
+
+from geospark.register import upload_jars, GeoSparkRegistrator
+from geospark.utils import KryoSerializer, GeoSparkKryoRegistrator
+
+# REQUIRED FOR DEBUGGING IN IntelliJ/Pycharm ONLY - comment out if running from command line
+# set Conda environment vars for PySpark
+os.environ["JAVA_HOME"] = "/Library/Java/Home"
+os.environ["SPARK_HOME"] = "/Users/s57405/spark-2.4.6-bin-hadoop2.7"
+os.environ["SPARK_LOCAL_IP"] = "127.0.0.1"
+os.environ["PYSPARK_PYTHON"] = "/Users/s57405/opt/miniconda3/envs/geospark_env/bin/python"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "/Users/s57405/opt/miniconda3/envs/geospark_env/bin/python"
+os.environ["PYLIB"] = os.environ["SPARK_HOME"] + "/python/lib"
+os.environ["PATH"] = "/Users/s57405/spark-2.4.6-bin-hadoop2.7/bin:" + os.environ["PATH"]
+
+
+# set postgres connection parameters
+def get_password(connection_name):
+    # get credentials from local file
+    passwords_file_path = os.path.join(os.environ["GIT_HOME"], "passwords.ini")
+
+    if os.path.exists(passwords_file_path):
+        passwords_file = open(passwords_file_path,'r').read().splitlines()
+        passwords_file = [i for i in passwords_file if len(i) != 0]  # remove empty lines
+        passwords_file = [i for i in passwords_file if i[0] != "#"]  # remove comment lines
+
+        params = dict()
+        for ini in passwords_file:
+            params[ini.split()[0].rstrip().lstrip()] = ini.split(':=')[1].rstrip().lstrip()
+
+        return dict(item.split("|") for item in params[connection_name].split(","))
+
+
+# get parameters for local Postgres server
+local_pg_settings = get_password("localhost_super")
+
+# create postgres JDBC url and properties
+jdbc_url = "jdbc:postgresql://{HOST}:{PORT}/{DB}".format(**local_pg_settings)
+
+# output path for gzipped CSV files
+output_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+
+
+def main():
+    start_time = datetime.now()
+
+    # upload Sedona (geospark) JARs
+    upload_jars()
+
+    spark = SparkSession \
+        .builder \
+        .master("local[*]") \
+        .appName("query") \
+        .config("spark.sql.session.timeZone", "UTC") \
+        .config("spark.sql.debug.maxToStringFields", 100) \
+        .config("spark.serializer", KryoSerializer.getName) \
+        .config("spark.kryo.registrator", GeoSparkKryoRegistrator.getName) \
+        .config("spark.sql.adaptive.enabled", "true") \
+        .config("spark.executor.cores", 1) \
+        .config("spark.cores.max", cpu_count() * 2) \
+        .config("spark.driver.memory", "8g") \
+        .config("spark.driver.maxResultSize", "1g") \
+        .getOrCreate()
+
+    # Register Apache Sedona (geospark) UDTs and UDFs
+    GeoSparkRegistrator.registerAll(spark)
+
+    logger.info("\t - PySpark {} session initiated: {}".format(spark.sparkContext.version, datetime.now() - start_time))
+    start_time = datetime.now()
+
+    # load ABS remoteness areas
+    sql = """select ra_code16 as bdy_id, st_astext(geom) as wkt_geom 
+             from census_2016_bdys.ra_2016_aust_analysis
+             where geom is not null"""
+    ra_df = get_dataframe_from_postgres(spark, sql)
+
+    # write remoteness areas to gzipped parquet
+    export_to_parquet(ra_df, "boundaries")
+
+    # load meshblock centroid coordinates (not geoms)
+    sql = """select mb_code16 as point_id, st_y(st_centroid(geom)) as latitude, st_x(st_centroid(geom)) as longitude 
+             from census_2016_bdys.mb_2016_aust
+             where geom is not null"""
+    mb_df = get_dataframe_from_postgres(spark, sql)
+
+    # write meshblock coords to gzipped parquet
+    export_to_parquet(mb_df, "points")
+
+    # cleanup
+    spark.stop()
+
+    logger.info("\t - Point and boundary data exported to gzipped parquet files: {}"
+                .format(datetime.now() - start_time))
+
+
+def get_dataframe_from_postgres(spark, sql):
+    df = spark.read.format("jdbc") \
+        .option("url", jdbc_url) \
+        .option("query", sql) \
+        .option("properties", local_pg_settings["USER"]) \
+        .option("password", local_pg_settings["PASS"]) \
+        .option("driver", "org.postgresql.Driver") \
+        .load()
+    return df
+
+
+def export_to_parquet(df, name):
+    df.write.option("compression", "gzip") \
+        .mode("overwrite") \
+        .parquet(os.path.join(output_path, name))
+
+
+if __name__ == "__main__":
+    full_start_time = datetime.now()
+
+    # setup logging
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    # set Spark logging levels
+    logging.getLogger("pyspark").setLevel(logging.ERROR)
+    logging.getLogger("py4j").setLevel(logging.ERROR)
+
+    # set logger
+    log_file = os.path.abspath(__file__).replace(".py", ".log")
+    logging.basicConfig(filename=log_file, level=logging.DEBUG, format="%(asctime)s %(message)s",
+                        datefmt="%m/%d/%Y %I:%M:%S %p")
+
+    # setup logger to write to screen as well as writing to log file
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter("%(name)-12s: %(levelname)-8s %(message)s")
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger("").addHandler(console)
+
+    task_name = "Geospark testing"
+    system_name = "mobility.ai"
+
+    logger.info("{} started".format(task_name))
+    logger.info("Running on Python {}".format(sys.version.replace("\n", " ")))
+
+    main()
+
+    time_taken = datetime.now() - full_start_time
+    logger.info("{} finished : {}".format(task_name, time_taken))
+    print()
