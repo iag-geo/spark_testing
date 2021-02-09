5bf3d5475084aec4bf5ad54810fc17b1bb0a84fe
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-02-09 12:53:13 +1100

    Updated install instructions

--- apache_sedona/xx_code_scraps.sh
+++ apache_sedona/xx_code_scraps.sh
@@ -1,9 +1,9 @@
 
 # 1. build Conda environment with Spark, Sedona and Jupyter
-. ~/git/iag_geo/spark_testing/apache_sedona/spark_3_testing/01_setup_geospark_3.sh
+. ~/git/iag_geo/spark_testing/apache_sedona/01_setup_sedona.sh
 
 # 2. go to script dir
-cd ~/git/iag_geo/spark_testing/apache_sedona/spark_3_testing
+cd ~/git/iag_geo/spark_testing/apache_sedona
 
 # 3. fire up Conda env
 conda activate sedona
