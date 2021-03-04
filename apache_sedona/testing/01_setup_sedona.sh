c1881e82c86c7df3c01ae4afb03066df47779572
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-03-04 21:11:50 +1100

    Working on nightly build setup

--- apache_sedona/testing/01_setup_sedona.sh
+++ apache_sedona/testing/01_setup_sedona.sh
@@ -14,7 +14,7 @@ echo " Start time : $(date)"
 #
 # WARNINGS:
 #   - Removes existing Spark install in $HOME/spark-$SPARK_VERSION-with-sedona folder
-#   - Removes existing 'sedona' Conda environment
+#   - Removes existing 'sedona-nightly' Conda environment
 #
 # PRE_REQUISITES:
 #   1. Java 8 OpenJDK is installed
@@ -41,13 +41,14 @@ echo " Start time : $(date)"
 
 PYTHON_VERSION="3.9"
 SPARK_VERSION="3.0.2"
+SEDONA_INSTALL_DIR="${HOME}/incubator-sedona"
 
 # --------------------------------------------------------------------------------------------------------------------
 
 # get the directory this script is running from
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-SPARK_HOME_DIR="${HOME}/spark-${SPARK_VERSION}-with-sedona"
+SPARK_HOME_DIR="${HOME}/spark-${SPARK_VERSION}-with-sedona-nightly"
 
 # WARNING - remove existing spark install
 rm -r ${SPARK_HOME_DIR}
@@ -91,23 +92,23 @@ mkdir -p ${HOME}/tmp/spark
 cd ${HOME} || exit
 
 echo "-------------------------------------------------------------------------"
-echo "Creating new Conda Environment 'sedona'"
+echo "Creating new Conda Environment 'sedona-nightly'"
 echo "-------------------------------------------------------------------------"
 
 # stop the Conda environment currently running
 conda deactivate
 
 # WARNING - remove existing environment
-conda env remove --name sedona
+conda env remove --name sedona-nightly
 
 # update Conda platform
 echo "y" | conda update conda
 
 # Create Conda environment
-echo "y" | conda create -n sedona python=${PYTHON_VERSION}
+echo "y" | conda create -n sedona-nightly python=${PYTHON_VERSION}
 
 # activate and setup env
-conda activate sedona
+conda activate sedona-nightly
 conda config --env --add channels conda-forge
 conda config --env --set channel_priority strict
 
@@ -116,21 +117,27 @@ conda env config vars set JAVA_HOME="/usr/local/opt/openjdk@8"
 conda env config vars set SPARK_HOME="${SPARK_HOME_DIR}"
 conda env config vars set SPARK_LOCAL_IP="127.0.0.1"
 conda env config vars set SPARK_LOCAL_DIRS="${HOME}/tmp/spark"
-conda env config vars set PYSPARK_PYTHON="${HOME}/opt/miniconda3/envs/sedona/bin/python"
-conda env config vars set PYSPARK_DRIVER_PYTHON="${HOME}/opt/miniconda3/envs/sedona/bin/ipython"
+conda env config vars set PYSPARK_PYTHON="${HOME}/opt/miniconda3/envs/sedona-nightly/bin/python"
+conda env config vars set PYSPARK_DRIVER_PYTHON="${HOME}/opt/miniconda3/envs/sedona-nightly/bin/ipython"
 conda env config vars set PYLIB="${SPARK_HOME_DIR}/python/lib"
 
 # reactivate for env vars to take effect
-conda activate sedona
+conda activate sedona-nightly
 
 # install conda packages for Sedona
 echo "y" | conda install -c conda-forge pyspark=${SPARK_VERSION} pyspark-stubs psycopg2 geopandas jupyter matplotlib
 
 echo "-------------------------------------------------------------------------"
-echo "Install Apache Sedona"
+echo "Build & Install Apache Sedona"
 echo "-------------------------------------------------------------------------"
 
-pip install apache-sedona
+
+
+# Copy Sedona JARs over to Spark install and install Sedona in Python from local setup.py
+cp ${SEDONA_INSTALL_DIR}/python-adapter/target/sedona-python-adapter-3.0_2.12-1.0.0-incubating.jar ${SPARK_HOME}/jars
+
+cd ${SEDONA_INSTALL_DIR}/python || exit
+python setup.py install
 
 echo "-------------------------------------------------------------------------"
 echo "Verify Sedona version"
