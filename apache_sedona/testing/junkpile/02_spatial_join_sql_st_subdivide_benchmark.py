6092b09826772942c8c3ca41521da64d1bf8cd61
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-03-16 17:50:15 +1100

    testing different sized polygon performance

--- apache_sedona/testing/junkpile/02_spatial_join_sql_st_subdivide_benchmark.py
+++ apache_sedona/testing/junkpile/02_spatial_join_sql_st_subdivide_benchmark.py
@@ -46,11 +46,11 @@ bdy_name = "commonwealth_electorates"
 bdy_id = "ce_pid"
 
 # bdy table subdivision vertex limit
-max_vertices_list = [150, 200, 250]
+max_vertices_list = [100, 150, 200, 250]
 
 # number of partitions on both dataframes
 # num_partitions_list = [150, 200, 250, 300, 350, 400, 450]
-num_partitions_list = [600, 700, 800, 900, 1000]
+num_partitions_list = [500, 1000, 1500, 2000]
 
 # output path for gzipped parquet files
 output_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data")
