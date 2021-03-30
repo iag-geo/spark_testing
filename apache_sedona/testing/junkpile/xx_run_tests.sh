41e8fbd1271ce8e358ce2d03ff8c93cb7238b298
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-03-31 08:40:16 +1100

    testing SQL versus RDD spatial join

--- apache_sedona/testing/junkpile/xx_run_tests.sh
+++ apache_sedona/testing/junkpile/xx_run_tests.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 
-cd /Users/${whoami}/git/iag_geo/spark_testing/apache_sedona/testing/junkpile
+cd /Users/$(whoami)/git/iag_geo/spark_testing/apache_sedona/testing/junkpile
 
 python 02_spatial_join_rdd_st_subdivide_benchmark.py
 python 02_spatial_join_sql_st_subdivide_benchmark.py
