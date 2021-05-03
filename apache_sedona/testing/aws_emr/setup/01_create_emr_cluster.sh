aefbedfea36a96f3714331537a7ce0a13c325d87
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-05-03 15:52:39 +1000

    improved EMR process (still incomplete)

--- apache_sedona/testing/aws_emr/setup/01_create_emr_cluster.sh
+++ apache_sedona/testing/aws_emr/setup/01_create_emr_cluster.sh
@@ -32,52 +32,67 @@ SECONDS=0*
 #
 # --------------------------------------------------------------------------------------------------------------------
 #
-# EDIT YOUR SETUP:
-
-SSH_CONFIG_FILE="${HOME}/.ssh/aws-emr-config"
-KEYPAIR_NAME="<your EC2 keypair name>"
-LOGFILE_S3_BUCKET="<your EMR log file S3 bucket name>"
-EC2_INSTANCE_TYPE="m5.xlarge"
-NUMBER_OF_CORE_SERVERS=4
-
-# --------------------------------------------------------------------------------------------------------------------
-
-echo "-------------------------------------------------------------------------"
-echo " Start time : $(date)"
 
 echo "-------------------------------------------------------------------------"
 echo " Set temp local environment vars"
 echo "-------------------------------------------------------------------------"
 
+SSH_CONFIG="${HOME}/.ssh/aws-sandbox-emr-config"
+
 # get directory this script is running from
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# load AWS parameters
+. ${HOME}/.aws/ec2_vars.sh
+
+echo "-------------------------------------------------------------------------"
+echo " Copy bootstrap bash file to S3 (adds additional Python packages to EMR)"
+echo "-------------------------------------------------------------------------"
+
+aws s3 cp ${SCRIPT_DIR}/emr_bootstrap.sh s3://${AWS_S3_BUCKET}/emr_bootstrap.sh
 
 echo "-------------------------------------------------------------------------"
 echo " Create EMR cluster, wait for startup and get cluster & master server IDs"
 echo "-------------------------------------------------------------------------"
 
+INSTANCE_TYPE="r5d.2xlarge"
+INSTANCE_COUNT=4
+
+EC2_ATTRIBUTES="{\"KeyName\":\"${AWS_KEYPAIR}\",\"InstanceProfile\":\"EMR_EC2_DefaultRole\",\"ServiceAccessSecurityGroup\":\"${AWS_EMR_SERVICE_SG}\",\"SubnetId\":\"${AWS_SUBNET}\",\"EmrManagedSlaveSecurityGroup\":\"${AWS_EMR_NODE_SG}\",\"EmrManagedMasterSecurityGroup\":\"${AWS_EMR_MASTER_SG}\"}"
+INSTANCE_GROUPS="[{\"InstanceCount\":${INSTANCE_COUNT},\"BidPrice\":\"OnDemandPrice\",\"InstanceGroupType\":\"CORE\",\"InstanceType\":\"${INSTANCE_TYPE}\",\"Name\":\"Core - 2\"},{\"InstanceCount\":1,\"BidPrice\":\"OnDemandPrice\",\"InstanceGroupType\":\"MASTER\",\"InstanceType\":\"${INSTANCE_TYPE}\",\"Name\":\"Master - 1\"}]"
+#echo "${EC2_ATTRIBUTES}"
+#echo ""
+#echo "${INSTANCE_GROUPS}"
+#echo ""
+
 CLUSTER_ID=$(aws emr create-cluster \
 --termination-protected \
---applications Name=Hadoop Name=Hive Name=Hue Name=Spark Name=JupyterHub \
---ec2-attributes '{"KeyName":"${KEYPAIR_NAME}","InstanceProfile":"EMR_EC2_DefaultRole","ServiceAccessSecurityGroup":"sg-0b55cd16d5595217c","SubnetId":"subnet-00e994fe9f63d9dbd","EmrManagedSlaveSecurityGroup":"sg-027b04b3dc9114d1c","EmrManagedMasterSecurityGroup":"sg-01e12be997b4a697a"}' \
+--applications Name=Hadoop Name=Hive Name=Spark \
+--ec2-attributes "${EC2_ATTRIBUTES}" \
 --release-label emr-6.2.0 \
---log-uri 's3n://${LOGFILE_S3_BUCKET}/elasticmapreduce/' \
---instance-groups '[{"InstanceCount":${NUMBER_OF_CORE_SERVERS},"BidPrice":"OnDemandPrice","InstanceGroupType":"CORE","InstanceType":"${EC2_INSTANCE_TYPE}","Name":"Core - 2"},{"InstanceCount":1,"BidPrice":"OnDemandPrice","InstanceGroupType":"MASTER","InstanceType":"${EC2_INSTANCE_TYPE}","Name":"Master - 1"}]' \
---configurations '[{"Classification":"spark","Properties":{"maximizeResourceAllocation":"true"}}]' \
+--log-uri "s3n://${AWS_S3_BUCKET}/elasticmapreducelogs/" \
+--instance-groups "${INSTANCE_GROUPS}" \
+--configurations "[{\"Classification\":\"spark\",\"Properties\":{\"maximizeResourceAllocation\":\"true\"}}]" \
+--bootstrap-actions Path="s3://${AWS_S3_BUCKET}/emr_bootstrap.sh" \
 --auto-scaling-role EMR_AutoScaling_DefaultRole \
 --ebs-root-volume-size 100 \
 --service-role EMR_DefaultRole \
 --enable-debugging \
---name 'compass_iot_processing' \
+--name "mobility_ai_processing" \
 --scale-down-behavior TERMINATE_AT_TASK_COMPLETION \
 --region ap-southeast-2 \
 --no-verify-ssl | \
 python3 -c "import sys, json; print(json.load(sys.stdin)['ClusterId'])")
 
-# wait for cluster to startup
+#--bootstrap-actions Path="s3://elasticmapreduce/bootstrap-actions/run-if", Args=["instance.isMaster=true","echo running on master node"], Path="s3://mobai-sandpit-bucket-compassiot-oem/emr_setup/remote_setup.sh", Name="remote_setup_script" \
+
+# SP's test custom AMI
+#--custom-ami-id ami-00851a17767e02d95 \
+
+# wait for it to startup
 aws emr wait cluster-running --cluster-id ${CLUSTER_ID}
 
-# get EC2 instance id & IP address of master server
+# get instance id of master server
 INSTANCE_GROUP_ID=$(aws emr describe-cluster --cluster-id ${CLUSTER_ID} | \
 python3 -c "import sys, json; print([i for i in json.load(sys.stdin)['Cluster']['InstanceGroups'] if i['InstanceGroupType'] == 'MASTER'][0]['Id'])")
 
@@ -85,26 +100,32 @@ INSTANCE_ID=$(aws emr list-instances --cluster-id ${CLUSTER_ID} --instance-group
 python3 -c "import sys, json; print(json.load(sys.stdin)['Instances'][0]['Ec2InstanceId'])")
 
 INSTANCE_IP_ADDRESS=$(aws emr list-instances --cluster-id ${CLUSTER_ID} --instance-group-id ${INSTANCE_GROUP_ID} | \
-python3 -c "import sys, json; print(json.load(sys.stdin)['Instances'][0]['PrivateIpAddress'])")
+python3 -c "import sys, json; print(json.load(sys.stdin)['Instances'][0]['PublicIpAddress'])")
 
 echo "-------------------------------------------------------------------------"
-echo " Copy setup script for master server"
+echo " Copy setup script for master server and run it remotely"
 echo "-------------------------------------------------------------------------"
 
 # also get rid of fingerprint check on trusted server
-scp -F ${SSH_CONFIG_FILE} -o StrictHostKeyChecking=no ${SCRIPT_DIR}/remote_setup.sh hadoop@${INSTANCE_ID}:~/
+scp -i ${AWS_KEYPAIR} -o StrictHostKeyChecking=no ${SCRIPT_DIR}/remote_setup.sh hadoop@${INSTANCE_IP_ADDRESS}:~/
+
+# run remote setup script
+ssh -i ${AWS_KEYPAIR} hadoop@${INSTANCE_IP_ADDRESS} ". ./remote_setup.sh"
 
 echo "-------------------------------------------------------------------------"
 echo " Copy python & sql scripts"
 echo "-------------------------------------------------------------------------"
 
-scp -F ${SSH_CONFIG_FILE} ${SCRIPT_DIR}/../*/*.py hadoop@${INSTANCE_ID}:~/
-scp -F ${SSH_CONFIG_FILE} ${SCRIPT_DIR}/../*/*.sql hadoop@${INSTANCE_ID}:~/
+scp -i ${AWS_KEYPAIR} ${SCRIPT_DIR}/../*/*.py hadoop@${INSTANCE_IP_ADDRESS}:~/
+scp -i ${AWS_KEYPAIR} ${SCRIPT_DIR}/../*/*.sql hadoop@${INSTANCE_IP_ADDRESS}:~/
 
 echo "----------------------------------------------------------------------------------------------------------------"
 
 # port forward Spark UI web site
-ssh -F ${SSH_CONFIG_FILE} -fNL 4040:${INSTANCE_IP_ADDRESS}:18080 ${INSTANCE_ID}
+ssh -i ${AWS_KEYPAIR} -fNL 4040:${INSTANCE_IP_ADDRESS}:18080 ${INSTANCE_IP_ADDRESS}
+
+## port forward Jupyter notebook
+#ssh -i ${AWS_KEYPAIR} -fNL 8889:${INSTANCE_IP_ADDRESS}:8889 ${INSTANCE_IP_ADDRESS}
 
 duration=$SECONDS
 
@@ -113,32 +134,31 @@ echo " EMR cluster build took $((duration / 60)) mins"
 echo "----------------------------------------------------------------------------------------------------------------"
 
 echo "Cluster ID  : ${CLUSTER_ID}"
-echo "Instance ID : ${INSTANCE_ID}"
+echo "Instance ID : ${INSTANCE_IP_ADDRESS}"
 echo "Instance IP : ${INSTANCE_IP_ADDRESS}"
 
-# save vars to local file for use in case you close Terminal accidentally
+# save vars to local file
 echo "export SCRIPT_DIR=${SCRIPT_DIR}" > ~/git/temp_master_server_vars.sh
-echo "export SSH_CONFIG_FILE=${SSH_CONFIG_FILE}" >> ~/git/temp_master_server_vars.sh
+echo "export SSH_CONFIG=${SSH_CONFIG}" >> ~/git/temp_master_server_vars.sh
 echo "export CLUSTER_ID=${CLUSTER_ID}" >> ~/git/temp_master_server_vars.sh
 echo "export INSTANCE_ID=${INSTANCE_ID}" >> ~/git/temp_master_server_vars.sh
 echo "export INSTANCE_IP_ADDRESS=${INSTANCE_IP_ADDRESS}" >> ~/git/temp_master_server_vars.sh
 
 echo "----------------------------------------------------------------------------------------------------------------"
 
-# connect to the EMR instance
-ssh -F ${SSH_CONFIG_FILE} ${INSTANCE_ID}
-
+## load Python env to enable copying files from S3 to local Postgres
+#conda activate minus34
 
 # USEFUL COMMANDS
 #
 ## connect to master server
-# ssh -F ${SSH_CONFIG_FILE} ${INSTANCE_ID}
+# ssh -i ${AWS_KEYPAIR} ${INSTANCE_IP_ADDRESS}
 #
 ## copy python scripts to remote
-# scp -F ${SSH_CONFIG_FILE} ${SCRIPT_DIR}/../*.py hadoop@${INSTANCE_ID}:~/
+# scp -i ${AWS_KEYPAIR} ${SCRIPT_DIR}/../*.py hadoop@${INSTANCE_IP_ADDRESS}:~/
 #
 ## port forward Spark UI web site (view it at http://localhost:4040)
-# ssh -F ${SSH_EC2_USER_CONFIG} -fNL 4040:${INSTANCE_IP_ADDRESS}:18080 ${INSTANCE_ID}
+# ssh -F ${SSH_EC2_USER_CONFIG} -fNL 4040:${INSTANCE_IP_ADDRESS}:18080 ${INSTANCE_IP_ADDRESS}
 #
 ## shutdown cluster
 # aws emr terminate-clusters --cluster-ids ${CLUSTER_ID}
---
+++ apache_sedona/testing/aws_emr/setup/emr_bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# install packages
+sudo pip-3 install requests
+sudo pip-3 install pyarrow==1.0.1
+sudo pip-3 install pandas
+sudo pip-3 install boto3
+sudo pip-3 install awscli
+sudo pip-3 install apache-sedona
