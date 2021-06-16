7745ec9dd915110d30d405a8e40a0337d797ac2c
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-06-16 21:05:09 +1000

    fixed sample export

--- utilities/converters/xx_parquet_to_postgres_example.sh
+++ utilities/converters/xx_parquet_to_postgres_example.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+conda activate sedona
+
 # test export from AWS S3 to local postgres (ABS Meshblock 2016 boundaries)
 python3 /Users/s57405/git/iag_geo/spark_testing/utilities/converters/parquet_to_postgres.py \
---aws-profile minus34 \
+--aws-profile default \
 --source-s3-bucket minus34.com \
 --source-s3-folder opendata/geoscape-202105/parquet/abs_2016_mb \
---geom-field ewkt_geom \
---geom-type MULIPOLYGON \
+--geom-field wkt_geom \
+--geom-type POLYGON \
 --srid 4283 \
 --target-table testing.abs_2016_mb
