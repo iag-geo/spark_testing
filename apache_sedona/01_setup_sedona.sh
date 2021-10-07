09b190e0226f516508ac5d625ed2690a1373e3cd
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-10-07 14:15:49 +1100

    Upgraded to Sedona 1.1.0 & simplified install process

--- apache_sedona/01_setup_sedona.sh
+++ apache_sedona/01_setup_sedona.sh
@@ -13,7 +13,6 @@ echo " Start time : $(date)"
 # Date: 2020-09-25
 #
 # WARNINGS:
-#   - Removes existing Spark install in $HOME/spark-$SPARK_VERSION-with-sedona folder
 #   - Removes existing 'sedona' Conda environment
 #
 # PRE_REQUISITES:
@@ -30,39 +29,69 @@ echo " Start time : $(date)"
 #
 # SETUP:
 #   - edit these if it's now the future and versions have changed
-#   - note: check which Spark versions Sedona supports
 #
 
 PYTHON_VERSION="3.9"
-SPARK_VERSION="3.1.2"
-POSTGRES_JDBC_VERSION="42.2.23"
-SEDONA_VERSION="1.0.1"
+SEDONA_VERSION="1.1.0"
 GEOTOOLS_VERSION="24.1"
+POSTGRES_JDBC_VERSION="42.2.23"
 
 # --------------------------------------------------------------------------------------------------------------------
 
 # get the directory this script is running from
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-SPARK_HOME_DIR="${HOME}/spark-${SPARK_VERSION}-with-sedona"
+# this assumes miniconda is installed in the default directory on linux/MacOS
+SPARK_HOME_DIR="${HOME}/opt/miniconda3/envs/sedona/lib/python${PYTHON_VERSION}/site-packages/pyspark"
 
-# WARNING - remove existing spark install
-rm -r ${SPARK_HOME_DIR}
+cd ${HOME}
 
 echo "-------------------------------------------------------------------------"
-echo "Downloading and Installing Apache Spark"
+echo "Creating new Conda Environment 'sedona'"
 echo "-------------------------------------------------------------------------"
 
-mkdir ${SPARK_HOME_DIR}
-cd ${SPARK_HOME_DIR}
+# stop the current Conda environment (if any running)
+conda deactivate
 
-# download and untar Spark files
-curl -O https://apache.mirror.digitalpacific.com.au/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoop3.2.tgz
-tar -xzf spark-${SPARK_VERSION}-bin-hadoop3.2.tgz --directory ${SPARK_HOME_DIR} --strip-components=1
-rm spark-${SPARK_VERSION}-bin-hadoop3.2.tgz
+# WARNING - remove existing environment
+conda env remove --name sedona
 
-# create log4j properties file (based on Spark template)
-cp ${SPARK_HOME_DIR}/conf/log4j.properties.template ${SPARK_HOME_DIR}/conf/log4j.properties
+# update Conda platform
+conda update -y conda
+
+# create Conda environment
+conda create -y -n sedona python=${PYTHON_VERSION}
+
+# activate env
+conda activate sedona
+
+# add environment variables for Pyspark
+conda env config vars set JAVA_HOME="/usr/local/opt/openjdk@11"
+conda env config vars set SPARK_HOME="${SPARK_HOME_DIR}"
+conda env config vars set SPARK_LOCAL_IP="127.0.0.1"
+conda env config vars set SPARK_LOCAL_DIRS="${HOME}/tmp/spark"
+conda env config vars set PYSPARK_PYTHON="${HOME}/opt/miniconda3/envs/sedona/bin/python${PYTHON_VERSION}"
+conda env config vars set PYSPARK_DRIVER_PYTHON="${HOME}/opt/miniconda3/envs/sedona/bin/ipython3"
+conda env config vars set PYLIB="${SPARK_HOME_DIR}/python/lib"
+
+# reactivate for env vars to take effect
+conda activate sedona
+
+# install supporting & useful packages
+conda install -y -c conda-forge psycopg2 sqlalchemy geoalchemy2 geopandas pyarrow jupyter matplotlib
+
+## OPTIONAL - AWS Packages
+#conda install -y -c conda-forge boto3 s3fs
+
+echo "-------------------------------------------------------------------------"
+echo "Install Pyspark with Apache Sedona"
+echo "-------------------------------------------------------------------------"
+
+# includes full ApacheSpark install
+pip install apache-sedona[spark]
+
+# create folder for Spark temp files
+mkdir -p ${HOME}/tmp/spark
 
 echo "-------------------------------------------------------------------------"
 echo "Downloading additional JAR files"
@@ -70,12 +99,12 @@ echo "-------------------------------------------------------------------------"
 
 cd ${SPARK_HOME_DIR}/jars
 
-# add Postgres JDBC driver to Spark (optional - included for running xx_prep_abs_boundaries.py)
-curl -O https://jdbc.postgresql.org/download/postgresql-${POSTGRES_JDBC_VERSION}.jar
-
 # add Apache Sedona Python shaded JAR and GeoTools
 curl -O https://repo1.maven.org/maven2/org/apache/sedona/sedona-python-adapter-3.0_2.12/${SEDONA_VERSION}-incubating/sedona-python-adapter-3.0_2.12-${SEDONA_VERSION}-incubating.jar
-curl -O https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/geotools-${GEOTOOLS_VERSION}/geotools-wrapper-geotools-${GEOTOOLS_VERSION}.jar
+curl -O https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/${SEDONA_VERSION}-${GEOTOOLS_VERSION}/geotools-wrapper-${SEDONA_VERSION}-${GEOTOOLS_VERSION}.jar
+
+# add Postgres JDBC driver to Spark (optional - included for running xx_prep_abs_boundaries.py)
+curl -O https://jdbc.postgresql.org/download/postgresql-${POSTGRES_JDBC_VERSION}.jar
 
 # get hadoop-aws & aws-java-sdk JAR files (optional - required for accessing AWS S3)
 #curl -O https://search.maven.org/remotecontent?filepath=org/apache/hadoop/hadoop-aws/3.2.0/hadoop-aws-3.2.0.jar
@@ -84,70 +113,18 @@ curl -O https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/geotools-
 # get Google Storage connector shaded JAR (optional - required for accessing GCP Storage)
 #curl -O https://search.maven.org/remotecontent?filepath=com/google/cloud/bigdataoss/gcs-connector/hadoop3-2.2.0/gcs-connector-hadoop3-2.2.0-shaded.jar
 
-# create folder for Spark temp files
-mkdir -p ${HOME}/tmp/spark
-
-cd ${HOME}
-
-echo "-------------------------------------------------------------------------"
-echo "Creating new Conda Environment 'sedona'"
-echo "-------------------------------------------------------------------------"
-
-# stop the Conda environment currently running
-conda deactivate
-
-# WARNING - remove existing environment
-conda env remove --name sedona
-
-# update Conda platform
-echo "y" | conda update conda
-
-# Create Conda environment
-echo "y" | conda create -n sedona python=${PYTHON_VERSION}
-
-# activate and setup env
-conda activate sedona
-conda config --env --add channels conda-forge
-conda config --env --set channel_priority strict
-
-# add environment variables for Pyspark
-conda env config vars set JAVA_HOME="/usr/local/opt/openjdk@11"
-conda env config vars set SPARK_HOME="${SPARK_HOME_DIR}"
-conda env config vars set SPARK_LOCAL_IP="127.0.0.1"
-conda env config vars set SPARK_LOCAL_DIRS="${HOME}/tmp/spark"
-conda env config vars set PYSPARK_PYTHON="${HOME}/opt/miniconda3/envs/sedona/bin/python"
-conda env config vars set PYSPARK_DRIVER_PYTHON="${HOME}/opt/miniconda3/envs/sedona/bin/ipython"
-conda env config vars set PYLIB="${SPARK_HOME_DIR}/python/lib"
-
-# reactivate for env vars to take effect
-conda activate sedona
-
-# install supporting & useful packages
-echo "y" | conda install -c conda-forge psycopg2 sqlalchemy geoalchemy2 geopandas pyarrow jupyter matplotlib
-
-## OPTIONAL - AWS Packages
-#echo "y" | conda install -c conda-forge boto3
-
-echo "-------------------------------------------------------------------------"
-echo "Install Apache Sedona"
-echo "-------------------------------------------------------------------------"
-
-pip install apache-sedona==${SEDONA_VERSION}
-
-# fix Sedona 1.0.1 packaging issue (uninstall Pyspark 3.0.2 and install required version)
-echo "y" | pip uninstall pyspark
-echo "y" | conda install -c conda-forge pyspark=${SPARK_VERSION}
-
 echo "-------------------------------------------------------------------------"
 echo "Verify Apache Spark and Sedona versions"
 echo "-------------------------------------------------------------------------"
 
-spark-submit --version
-echo "------------------------------------------------------"
-pip list | grep "spark"
-echo "------------------------------------------------------"
-pip list | grep "sedona"
+${SPARK_HOME_DIR}/bin/spark-submit --version
 
+echo "---------------------------------------"
+pip list | grep "spark"
+echo "---------------------------------------"
+pip list | grep "sedona"
+echo "---------------------------------------"
+echo ""
 echo "-------------------------------------------------------------------------"
 echo "Run test Sedona script to prove everything is working"
 echo "-------------------------------------------------------------------------"
