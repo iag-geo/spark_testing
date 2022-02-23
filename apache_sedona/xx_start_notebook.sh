a0abc00ed6a883e2ff048e3782a673daf97c3b27
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-02-23 16:14:32 +1100

    improved notebook startup

--- apache_sedona/xx_start_notebook.sh
+++ apache_sedona/xx_start_notebook.sh
@@ -1,3 +1,7 @@
-cd ~/git/iag_geo/spark_testing/apache_sedona
+
+# get the directory this script is running from
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd ${SCRIPT_DIR}
 conda activate sedona
 jupyter notebook
